32ceae61db66ba94ae92cc64ffe4e6665d8ce3da
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2024-05-17 20:38:16 +0530

    new loop funstion is added

---
+++ chapter6.js
@@ -0,0 +1,20 @@
+let obj = {
+    harry: 90,
+    shubh: 45,
+    shivika: 56,
+    ritika: 57,
+    shiv: 23
+  }
+  
+  // For in loop
+  for (let a in obj) {
+    console.log("Marks of " + a + " are " + obj[a]) // (a) is a keypiont if you  use [] braket it access under value
+  }
+  
+  // For of loop
+  for (let b of "harrash") 
+    // (of) use in arry with string
+    {
+    console.log(b)
+  }
+  
