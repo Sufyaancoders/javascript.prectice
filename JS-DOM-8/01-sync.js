ef784385925b6338fbca158f4b3f9f610d6f1249
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2024-11-08 16:28:27 +0530

    add promise point

--- JS-DOM-8/01-sync.js
+++ JS-DOM-8/01-sync.js
@@ -9,3 +9,14 @@ console.log("Hello");
 //
 //-----------------------------
 //API === application programming interface
+// promise === promise is an object that can run parllely background task
+// promise === resolve and reject function 
+ let point = new Promise((resolve, reject) => {
+     setTimeout(() => {
+        console.log("This is PRint after seconds");
+         
+     },400);
+     resolve(221);// reject(221);
+ })
+ console.log("first");
+ console.log(point);
